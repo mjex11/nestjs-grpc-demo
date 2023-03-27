f3c0590757eb85d5854714ef5cde6200a6f98b20
ygoto <37140182+mjex11@users.noreply.github.com>
2023-03-28 03:07:55 +0900

    Update grpcurl_sample_getUser.sh

--- script/grpcurl_sample_getUser.sh
+++ script/grpcurl_sample_getUser.sh
@@ -1,1 +1,6 @@
-grpcurl --import-path ~/lab/nestjs-grpc-demo/src -proto=proto/user.proto -plaintext -d '{"id": "2"}' localhost:5000 user.UserService/GetUser
+#!/bin/bash
+
+grpcurl --import-path ~/lab/nestjs-grpc-demo/src \
+-proto=proto/user.proto \
+-plaintext -d '{"id": "2"}' \
+localhost:5000 user.UserService/GetUser
